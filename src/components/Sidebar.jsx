e3448084d120ff005c0a3a16223aad4e4bf114c3
AzhuraaaReyy <rizalkurniawanramadhan52@gmail.com>
2026-02-07 15:04:23 +0700

    progress 11%

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,9 @@
+import { MdOutlineDashboard } from "react-icons/md";
+function Sidebar() {
+  return (
+    <div className="w-64 bg-red p-4 shadow-md">
+      <MdOutlineDashboard />Dashboard
+    </div>
+  );
+}
+export default Sidebar;
