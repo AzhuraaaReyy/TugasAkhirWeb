b50cacff270ba800c1344c2f9193b45a86198804
Muhammad Rizal <rizalkurniawanramadhan52@gmail.com>
2026-02-07 15:16:38 +0700

    Delete src directory

--- src/App.jsx
+++
@@ -1,8 +0,0 @@
-import Sidebar from "./components/Sidebar";
-import Navbar from "./components/Navbar";
-import Homepage from "./pages/homepage";
-function App() {
-  return <Homepage />;
-}
-
-export default App;
--- src/components/Sidebar.jsx
+++
@@ -1,9 +0,0 @@
-import { MdOutlineDashboard } from "react-icons/md";
-function Sidebar() {
-  return (
-    <div className="w-64 bg-red p-4 shadow-md">
-      <MdOutlineDashboard />Dashboard
-    </div>
-  );
-}
-export default Sidebar;
